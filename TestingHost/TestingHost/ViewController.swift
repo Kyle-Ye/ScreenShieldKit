151a34005107a609866255e39b0eb14db0598ba9
Kyle <kyle201817146@gmail.com>
2025-02-19 17:23:32 +0800

    Update ViewController

--- TestingHost/TestingHost/ViewController.swift
+++ TestingHost/TestingHost/ViewController.swift
@@ -9,30 +9,31 @@ import UIKit
 import ScreenShieldKit
 
 class ViewController: UIViewController {
-    let hideSubViewSwitch = UISwitch()
-    var subview = UIView()
-    
+    private lazy var hideFromCaptureSwitch: UISwitch = {
+        let switchView = UISwitch()
+        switchView.center = self.view.center
+        switchView.addTarget(self, action: #selector(toggleChanged(_:)), for: .valueChanged)
+        switchView.hideFromCapture()
+        return switchView
+    }()
+
+    private lazy var blueColorView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .blue
+        view.frame = self.view.bounds
+        return view
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         view.backgroundColor = .red
-        subview.backgroundColor = .blue
-        hideSubViewSwitch.backgroundColor = .darkGray
-        hideSubViewSwitch.layer.cornerRadius = hideSubViewSwitch.frame.height / 2
-        hideSubViewSwitch.hideFromCapture(hide: true)
-        
-        view.addSubview(subview)
-        view.addSubview(hideSubViewSwitch)
-        
-        subview.frame = view.frame
-        hideSubViewSwitch.center = view.center
-        
-        hideSubViewSwitch.addTarget(self, action: #selector(toggleHideFromCapture),
-                                    for: .valueChanged)
+        view.addSubview(blueColorView)
+        view.addSubview(hideFromCaptureSwitch)
     }
-    
-    @objc func toggleHideFromCapture() {
-        subview.hideFromCapture(hide: hideSubViewSwitch.isOn)
+
+    @objc
+    private func toggleChanged(_ sender: UISwitch) {
+        blueColorView.hideFromCapture(hide: sender.isOn)
     }
 
     override var prefersStatusBarHidden: Bool { true }
@@ -41,9 +42,7 @@ class ViewController: UIViewController {
 
     override func motionBegan(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
         if motion == .motionShake {
-            view.subviews.forEach {
-                $0.hideFromCapture()
-            }
+            hideFromCaptureSwitch.isOn.toggle()
         }
     }
 }
