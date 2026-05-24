3410f91665862fd1df3242dcf7f6c4106f6df8e9
Kyle-Ye <kyle201817146@gmail.com>
2026-05-25 01:48:48 +0800

    Lower package platform baselines

--- Package.swift
+++ Package.swift
@@ -5,11 +5,11 @@ import PackageDescription
 let package = Package(
     name: "ScreenShieldKit",
     platforms: [
-        .iOS(.v18),
-        .macOS(.v15),
-        .tvOS(.v18),
-        .watchOS(.v11),
-        .visionOS(.v2),
+        .iOS(.v13),
+        .macOS(.v10_15),
+        .tvOS(.v13),
+        .watchOS(.v6),
+        .visionOS(.v1),
     ],
     products: [
         .library(name: "ScreenShieldKit", targets: ["ScreenShieldKit"]),
